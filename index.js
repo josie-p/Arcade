fed2a29ea031a5996d57762f06cd44d2dc117b93
Your Name <parker.josie.eden@gmail.com>
2023-02-07 13:19:40 -0600

    getting ticks to display on screen alternately.

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ let displayName1 = document.getElementsByTagName('p')[0];
 
 let displayName2 = document.getElementsByTagName('p')[1];
 
+let displayTurn = document.getElementsByTagName('p')[2];
+
 let nameButton = document.getElementById('nameButton');
 
 let table = document.getElementsByTagName('table')[0];
+
+let td = document.getElementsByTagName('td');
 
 let gameState = {
     players: ['x', 'o'],
@@ -13,8 +17,17 @@ let gameState = {
         [null, null, null],
         [null, null, null], 
         [null, null, null]
-    ]
+    ], 
+    currentPlayer : ''
 };
+
+//randomly assign player an x or an o:
+function assignPlayer(){
+    let randInd =  Math.floor(Math.random() * 2);
+    gameState.currentPlayer = gameState.players[randInd];
+    displayTurn.innerText = `${gameState.currentPlayer}, it's your turn!`;
+    return randInd;
+  }
 
 function displayNames(){
     let name1 = document.getElementById('playerName1').value;
@@ -32,18 +45,42 @@ function displayNames(){
     displayName1.innerText = `${name1}, you are playing with the letter ${gameState.players[ind1]}`;
     displayName2.innerText = `${name2}, you are playing with the letter ${gameState.players[ind2]}`;
 
+    // console.log(gameState.currentPlayer);
+
+    // togglePlayer();
+
+    // console.log(gameState.currentPlayer);
+
 }
+
+// function setPlayer(num){
+//     gameState.currentPlayer = gameState.players[num];
+//     displayTurn.innerText = `${gameState.currentPlayer}, it's your turn!`;
+// }
 
 nameButton.addEventListener('click', displayNames);
 
-
-// table.addEventListener('click', function(evt){
-//     console.log(evt.target);
-//     // evt.target.innerText = 
-// })
-
-//randomly assign player an x or an o:
-function assignPlayer(){
-  let randInd =  Math.floor(Math.random() * 2);
-  return randInd;
+//toggle between players
+function togglePlayer(){
+  if( gameState.currentPlayer === gameState.players[0]){
+    gameState.currentPlayer = gameState.players[1];
+  }else{
+    gameState.currentPlayer = gameState.players[0];
+  }
 }
+
+table.addEventListener('click', function(evt){
+
+    if(gameState.currentPlayer === 'x'){
+        console.log(gameState.currentPlayer);
+        evt.target.innerText = 'X';
+    }else{
+        console.log(gameState.currentPlayer);
+        evt.target.innerText = 'O';
+    }
+
+    togglePlayer();
+    console.log(gameState.currentPlayer);
+
+    // console.log(td);
+})
